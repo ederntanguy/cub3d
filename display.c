af0f64a53cf2f7dd12a15cf9807b8252d8b8c404
etanguy <etanguy@student.42.fr>
2023-05-03 13:37:35 +0200

    premiere affichage un peu bancale

--- display.c
+++ display.c
@@ -16,10 +16,13 @@ void	show_right_left_line(t_img img, int pos_x, int height, int color)
 
 void	show_bottom_top_line(t_img img, t_blocK_wall all_wall, int color)
 {
-	double	len_line;
-	int		hgt_scrn;
-	double	radian;
+	double			len_line;
+	int				hgt_scrn;
+	double			radian;
+	int				i;
+	t_coordonate	pos;
 
+	i = 0;
 	(void) img;
 	(void) color;
 	hgt_scrn = HEIGHT_SCREEN;
@@ -27,6 +30,16 @@ void	show_bottom_top_line(t_img img, t_blocK_wall all_wall, int color)
 				- (hgt_scrn - all_wall.height_end) / 2, 2)
 				+ pow(all_wall.pos_x_start - all_wall.pos_x_end, 2));
 	radian = acos((all_wall.pos_x_end - all_wall.pos_x_start) / len_line);
+	while (i < len_line)
+	{
+		pos.x = i * cos(radian) + all_wall.pos_x_start;
+		pos.y = i * sin(radian) + (hgt_scrn - all_wall.height_start) / 2;
+		my_mlx_pixel_put(&img, pos.x, pos.y, color);
+		pos.x = i * cos(- radian) + all_wall.pos_x_start;
+		pos.y = i * sin(- radian) + (hgt_scrn - (hgt_scrn - all_wall.height_start) / 2);
+		my_mlx_pixel_put(&img, pos.x, pos.y, color);
+		i++;
+	}
 	printf("length = %lf rad = %lf\n", len_line, radian);
 }
 
