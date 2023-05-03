9439949ba66c35b4cc24ccd20f2d02fdb6a1c3cf
etanguy <etanguy@student.42.fr>
2023-05-03 12:32:49 +0200

    start calcule for top and bottom line

--- display.c
+++ display.c
@@ -1,6 +1,6 @@
 #include "cub3d.h"
 
-void show_management_line(t_img img, int pos_x, int height, int color)
+void	show_right_left_line(t_img img, int pos_x, int height, int color)
 {
 	int hgt_scrn;
 	int y;
@@ -14,7 +14,23 @@ void show_management_line(t_img img, int pos_x, int height, int color)
 	}
 }
 
-void show_screen(t_blocK_wall *all_wall, t_img img)
+void	show_bottom_top_line(t_img img, t_blocK_wall all_wall, int color)
+{
+	double	len_line;
+	int		hgt_scrn;
+	double	radian;
+
+	(void) img;
+	(void) color;
+	hgt_scrn = HEIGHT_SCREEN;
+	len_line = sqrt(pow((hgt_scrn - all_wall.height_start) / 2
+				- (hgt_scrn - all_wall.height_end) / 2, 2)
+				+ pow(all_wall.pos_x_start - all_wall.pos_x_end, 2));
+	radian = acos((all_wall.pos_x_end - all_wall.pos_x_start) / len_line);
+	printf("length = %lf rad = %lf\n", len_line, radian);
+}
+
+void	show_screen(t_blocK_wall *all_wall, t_img img)
 {
 	int i;
 	int color;
@@ -24,9 +40,10 @@ void show_screen(t_blocK_wall *all_wall, t_img img)
 	i = 0;
 	while (all_wall[i].wall_len != -1.0)
 	{
-		show_management_line(img, all_wall[i].pos_x_start, all_wall[i].height_start, color);
-		show_management_line(img, all_wall[i].pos_x_end - 1, all_wall[i].height_end, color);
+		show_right_left_line(img, all_wall[i].pos_x_start, all_wall[i].height_start, color);
+		show_right_left_line(img, all_wall[i].pos_x_end - 1, all_wall[i].height_end, color);
 		color *= 16;
+		show_bottom_top_line(img, all_wall[i], color);
 		i++;
 	}
 }
