e802b254c7aa1fe7f71f6f89163098beac0870e9
etanguy <etanguy@student.42.fr>
2023-05-05 12:05:19 +0200

    il faut calculer la haueteur de chaque mur en fonction du pan de mur

--- creat_wall_array.c
+++ creat_wall_array.c
@@ -1,73 +1,113 @@
 #include "cub3d.h"
 
+
+//int count_wall(t_raycast_info *raycast_info)
+//{
+//	int				count;
+//	int				i;
+//	int				evolution;
+//	t_raycast_info	last;
+//
+//	i = 0;
+//	count = 1;
+//	last = raycast_info[0];
+//	evolution = 0;
+//	while (raycast_info[i].distance != -1)
+//	{
+//		if (last.distance > raycast_info[i].distance && evolution == 1)
+//			count++;
+//		else if ((last.distance
+//				  < raycast_info[i].distance
+//				  && evolution == -1))
+//			count++;
+//		else if (last.distance >= raycast_info[i].distance
+//				 && last.distance - raycast_info[i].distance > LEN_CHUNCK / 2)
+//		{
+//			evolution = 0;
+//			count++;
+//			last = raycast_info[i++];
+//			continue ;
+//		}
+//		else if (last.distance < raycast_info[i].distance
+//				 && raycast_info[i].distance - last.distance > LEN_CHUNCK / 2)
+//		{
+//			evolution = 0;
+//			count++;
+//			last = raycast_info[i++];
+//			continue ;
+//		}
+//		if (last.distance > raycast_info[i].distance)
+//			evolution = -1;
+//		else if (last.distance < raycast_info[i].distance)
+//			evolution = 1;
+//		last = raycast_info[i++];
+//	}
+//	return (count);
+//}
+
 int count_wall(t_raycast_info *raycast_info)
 {
 	int				count;
 	int				i;
-	int				evolution;
-	t_raycast_info	last;
+	t_coordonate	last_segment;
 
 	i = 0;
 	count = 1;
-	last = raycast_info[0];
-	evolution = 0;
+	last_segment.x = raycast_info[0].pos.x / 100;
+	last_segment.y = raycast_info[0].pos.y / 100;
 	while (raycast_info[i].distance != -1)
 	{
-		if (last.distance > raycast_info[i].distance && evolution == 1)
-			count++;
-		else if ((last.distance
-		        < raycast_info[i].distance
-				&& evolution == -1))
-			count++;
-		else if (last.distance >= raycast_info[i].distance
-				&& last.distance - raycast_info[i].distance > LEN_CHUNCK / 2)
+		if (raycast_info[i].pos.x / 100 != last_segment.x || raycast_info[i].pos.y / 100 != last_segment.y)
 		{
-			evolution = 0;
 			count++;
-			last = raycast_info[i++];
-			continue ;
+			last_segment.x = raycast_info[i].pos.x / 100;
+			last_segment.y = raycast_info[i].pos.y / 100;
 		}
-		else if (last.distance < raycast_info[i].distance
-				 && raycast_info[i].distance - last.distance > LEN_CHUNCK / 2)
-		{
-			evolution = 0;
-			count++;
-			last = raycast_info[i++];
-			continue ;
-		}
-		if (last.distance > raycast_info[i].distance)
-			evolution = -1;
-		else if (last.distance < raycast_info[i].distance)
-			evolution = 1;
-		last = raycast_info[i++];
+		i++;
 	}
 	return (count);
 }
 
+//double get_end_wall(t_raycast_info *raycast_info, int *i)
+//{
+//	int		evolution;
+//	t_raycast_info	last;
+//
+//	last = raycast_info[*i];
+//	evolution = 0;
+//	while (raycast_info[*i].distance != -1)
+//	{
+//		if (last.distance > raycast_info[*i].distance && evolution == 1)
+//			return (raycast_info[*i - 1].distance);
+//		else if ((last.distance < raycast_info[*i].distance && evolution == -1))
+//			return (raycast_info[*i - 1].distance);
+//		else if (last.distance >= raycast_info[*i].distance
+//				 && last.distance - raycast_info[*i].distance > LEN_CHUNCK / 2)
+//			return (raycast_info[*i - 1].distance);
+//		else if (last.distance < raycast_info[*i].distance
+//				 && raycast_info[*i].distance - last.distance > LEN_CHUNCK / 2)
+//			return (raycast_info[*i - 1].distance);
+//		if (last.distance > raycast_info[*i].distance)
+//			evolution = -1;
+//		else if (last.distance < raycast_info[*i].distance)
+//			evolution = 1;
+//		last = raycast_info[(*i)++];
+//	}
+//	return (raycast_info[*i - 1].distance);
+//}
+
 double get_end_wall(t_raycast_info *raycast_info, int *i)
 {
-	int		evolution;
-	t_raycast_info	last;
+	t_coordonate	last_segment;
 
-	last = raycast_info[*i];
-	evolution = 0;
+	last_segment.x = raycast_info[*i].pos.x / 100;
+	last_segment.y = raycast_info[*i].pos.y / 100;
+
 	while (raycast_info[*i].distance != -1)
 	{
-		if (last.distance > raycast_info[*i].distance && evolution == 1)
-			return (raycast_info[*i - 1].distance);
-		else if ((last.distance < raycast_info[*i].distance && evolution == -1))
-			return (raycast_info[*i - 1].distance);
-		else if (last.distance >= raycast_info[*i].distance
-				 && last.distance - raycast_info[*i].distance > LEN_CHUNCK / 2)
-			return (raycast_info[*i - 1].distance);
-		else if (last.distance < raycast_info[*i].distance
-				 && raycast_info[*i].distance - last.distance > LEN_CHUNCK / 2)
-			return (raycast_info[*i - 1].distance);
-		if (last.distance > raycast_info[*i].distance)
-			evolution = -1;
-		else if (last.distance < raycast_info[*i].distance)
-			evolution = 1;
-		last = raycast_info[(*i)++];
+		if (raycast_info[*i].pos.x / 100 != last_segment.x || raycast_info[*i].pos.y / 100 != last_segment.y)
+			return (raycast_info[*i].distance);
+		(*i)++;
 	}
 	return (raycast_info[*i - 1].distance);
 }
@@ -126,21 +166,22 @@ void init_all_wall(t_blocK_wall *all_wall, t_raycast_info *raycast_info)
 				/ (all_wall[i].start_distance / 100);
 		all_wall[i].height_end = (int)(HEIGHT_SCREEN * 1.2)
 				/ (all_wall[i].end_distance / 100);
-		if (i > 0 && ((all_wall[i].last_inpact.x == all_wall[i].first_inpact.x
-					  && (all_wall[i].first_inpact.x == all_wall[i - 1].last_inpact.x)
-					  && (all_wall[i - 1].last_inpact.x == all_wall[i - 1].first_inpact.x))
-			|| ((all_wall[i].last_inpact.y == all_wall[i].first_inpact.y)
-				&& (all_wall[i].first_inpact.y == all_wall[i - 1].last_inpact.y)
-				&& (all_wall[i - 1].last_inpact.y == all_wall[i - 1].first_inpact.y))))
-		{
-			all_wall[i - 1] = fusion_wall(all_wall[i - 1], all_wall[i]);
-			nb_fusion++;
-			i--;
-		}
+//		if (i > 0 && ((all_wall[i].last_inpact.x == all_wall[i].first_inpact.x
+//					  && (all_wall[i].first_inpact.x == all_wall[i - 1].last_inpact.x)
+//					  && (all_wall[i - 1].last_inpact.x == all_wall[i - 1].first_inpact.x))
+//			|| ((all_wall[i].last_inpact.y == all_wall[i].first_inpact.y)
+//				&& (all_wall[i].first_inpact.y == all_wall[i - 1].last_inpact.y)
+//				&& (all_wall[i - 1].last_inpact.y == all_wall[i - 1].first_inpact.y))))
+//		{
+//			all_wall[i - 1] = fusion_wall(all_wall[i - 1], all_wall[i]);
+//			nb_fusion++;
+//			i--;
+//		}
 		i++;
 	}
-	while (nb_fusion > 0)
-		all_wall[i - nb_fusion-- + 1].wall_len = -1;
+//	while (nb_fusion > 0)
+//		all_wall[i - nb_fusion-- + 1].wall_len = -1;
+	all_wall[count_wall(raycast_info)].wall_len = -1;
 }
 
 t_blocK_wall	*creat_wall_array(t_data data, t_img img, t_raycast_info *raycast_info)
