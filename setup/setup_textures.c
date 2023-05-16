c43dfe4f3fb0890a0834e614273c5182f9e67d10
edern <edern56300@gmail.com>
2023-05-16 16:01:40 +0200

    abirtrai space un .cub

--- setup/setup_textures.c
+++ setup/setup_textures.c
@@ -3,9 +3,14 @@
 t_img	make_image(void *mlx, char *filename)
 {
 	t_img	img;
+	int 	i;
 
+	i = 0;
+	while (filename[i] && filename[i + 1]
+		&& filename[i + 2] && filename[i + 2] == ' ')
+		i++;
 	filename[ft_strlen(filename) - 1] = 0;
-	img.img = mlx_xpm_file_to_image(mlx, filename + 3, &(img.width), &(img.heigth));
+	img.img = mlx_xpm_file_to_image(mlx, filename + 2 + i, &(img.width), &(img.heigth));
 	img.addr = (unsigned char *)mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 					&img.endian);
 	return (img);
@@ -18,6 +23,7 @@ int	make_color(char *str)
 
 	ret = 0;
 	tmp = 0;
+
 	while (*str)
 	{
 		if (*str == ',' || *str == '\n')
@@ -26,7 +32,7 @@ int	make_color(char *str)
 			ret += tmp;
 			tmp = 0;
 		}
-		else
+		else if (*str != ' ')
 		{
 			tmp *= 10;
 			tmp += *str - '0';
@@ -38,25 +44,29 @@ int	make_color(char *str)
 
 int	setup_textures(int fd, t_textures *textures, t_window *window)
 {
-	char *tmp;
-	int	stop;
+	char	*tmp;
+	int		stop;
+	int 	i;
 
 	stop = 0;
 	tmp = get_next_line(fd);
 	while (tmp && stop < 6)
 	{
-		if (ft_strncmp(tmp, "NO ", 3) == 0 && ++stop)
-			textures->north = make_image(window->mlx, tmp);
-		else if (ft_strncmp(tmp, "SO ", 3) == 0 && ++stop)
-			textures->south = make_image(window->mlx, tmp);
-		else if (ft_strncmp(tmp, "WE ", 3) == 0 && ++stop)
-			textures->west = make_image(window->mlx, tmp);
-		else if (ft_strncmp(tmp, "EA ", 3) == 0 && ++stop)
-			textures->east = make_image(window->mlx, tmp);
-		else if (ft_strncmp(tmp, "F ", 2) == 0 && ++stop)
-			textures->floor = make_color(tmp + 2);
+		i = 0;
+		while (tmp[i] && tmp[i] == ' ')
+			i++;
+		if (ft_strncmp(tmp + i, "NO ", 3) == 0 && ++stop)
+			textures->north = make_image(window->mlx, tmp + i);
+		else if (ft_strncmp(tmp + i, "SO ", 3) == 0 && ++stop)
+			textures->south = make_image(window->mlx, tmp + i);
+		else if (ft_strncmp(tmp + i, "WE ", 3) == 0 && ++stop)
+			textures->west = make_image(window->mlx, tmp + i);
+		else if (ft_strncmp(tmp + i, "EA ", 3) == 0 && ++stop)
+			textures->east = make_image(window->mlx, tmp + i);
+		else if (ft_strncmp(tmp + i, "F ", 2) == 0 && ++stop)
+			textures->floor = make_color(tmp + 2 + i);
 		else if (ft_strncmp(tmp, "C ", 2) == 0 && ++stop)
-			textures->roof = make_color(tmp + 2);
+			textures->roof = make_color(tmp + 2 + i);
 		free(tmp);
 		tmp = get_next_line(fd);
 	}
