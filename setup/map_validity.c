eb3c18a2ac31444a3d8709965253b27ae5436591
Hybris207 <gaetan.decarvalho@outlook.fr>
2023-05-16 17:25:43 +0200

    Fixed parcing, made invalid character an error

--- setup/map_validity.c
+++ setup/map_validity.c
@@ -1,8 +1,28 @@
 #include "../cub3d.h"
 
+int	invalid_character(char **map)
+{
+	int	x;
+	int	y;
+
+	y = 0;
+	while (map[y])
+	{
+		x = 0;
+		while (map[y][x])
+		{
+			if (!(map[y][x] == '0' || map[y][x] == 'N' || map[y][x] == 'S' || map[y][x] == 'E' || map[y][x] == 'W' || map[y][x] == ' ' || map[y][x] == '1' || map[y][x] == '\n'))
+				return (1);
+			x++;
+		}
+		y++;
+	}
+	return (0);
+}
+
 int empty(char c)
 {
-	if (c == ' ')
+	if (c == ' ' || c == 0 || c == '\n')
 		return (1);
 	return (0);
 }
@@ -65,6 +85,8 @@ int	map_validity(char **map)
 {
 	if (!map)
 		return (ft_putstr_fd("Malloc of map failed\n", 2), 0);
+	if (invalid_character(map))
+		return (ft_putstr_fd("The Map countain Invalid characters\n", 2), 0);
 	if (missing_player(map))
 		return (ft_putstr_fd("Invalid number of player\n", 2), 0);
 	if (hole_in_map(map))
